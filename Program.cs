247fabd8a71e4e49ef12cc88e69dfbff5956cc52
User <User@W11PC>
2024-06-17 16:02:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Main1");
+        Task.Run(() => Parallel.Invoke(Method1, Method2));
+        //Thread.Sleep(1000);
+        Console.WriteLine("Main2");
+        Console.ReadLine();
+        static void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        static void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+}
